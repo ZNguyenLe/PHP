2ee17b017cb64772041aa23d4201c7154a046a69
ZNguyenLe <90646702+ZNguyenLe@users.noreply.github.com>
2021-10-17 20:47:21 -0400

    blog_oop assignment

---
+++ blog_oop/includes/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+require_once('config.php');
+
+// require_once('Database.php');
+// require_once('Session.php');
+// require_once('Category.php');
+// require_once('Entry.php');
+// require_once('Comment.php');
+// require_once('User.php');
+
+spl_autoload_register(function($class) {
+    $class = ucfirst($class);
+    $ext = '.php';
+    $file = __DIR__ . "/" . $class . $ext;
+        echo "Autoloading class $file <br>";
+        if (is_readable($file)){
+            require_once($file);
+        }
+    
+        
+});
+$dbc = new Database();
+$session = new Session();
+
+?>
